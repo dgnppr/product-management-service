5d4539b47aa708c2a69284914b58a5b3278fda72
Yonghyun Yoon <dgnppr@gmail.com>
2024-02-10 21:36:55 +0900

    feat<docker-compose>: 로컬 도커 테스트 환경 구성

--- module-infrastructure/persistence-database/src/main/java/me/dgpr/persistence/config/BaseEntity.java
+++ module-infrastructure/persistence-database/src/main/java/me/dgpr/persistence/config/BaseEntity.java
@@ -2,6 +2,8 @@ package me.dgpr.persistence.config;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import java.time.LocalDateTime;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -13,6 +15,18 @@ public abstract class BaseEntity {
     private LocalDateTime createdAt;
     private LocalDateTime lastModifiedAt;
     private LocalDateTime deletedAt;
+
+    @PrePersist
+    void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdAt = now;
+        lastModifiedAt = now;
+    }
+
+    @PreUpdate
+    void preUpdate() {
+        lastModifiedAt = LocalDateTime.now();
+    }
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
